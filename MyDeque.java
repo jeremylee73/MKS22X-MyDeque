0f34f9daed2601decb19ba11baca2ab5293c8c35
Jeremy Lee <jlee00@stuy.edu>
2019-03-21 14:20:39 -0400

    Added template

---
+++ MyDeque.java
@@ -0,0 +1,42 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  public MyDeque(){
+
+  }
+  public MyDeque(int initialCapacity){
+
+  }
+  public int size(){
+    return 0;
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public void addFirst(E element){
+
+  }
+
+  public void addLast(E element){
+
+  }
+
+  public E removeFirst(){
+    return data[0];
+  }
+
+  public E removeLast(){
+    return data[0];
+  }
+
+  public E getFirst(){
+    return data[0];
+  }
+
+  public E getLast(){
+    return data[0];
+  }
+}
