b3c2ec52eb5acec8d774e2133936237ccb3705e4
Jeremy Lee <jlee00@stuy.edu>
2019-03-25 14:35:29 -0400

    Added deque

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,17 @@ public class Calculator{
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
+      String[] vals = s.split(" ", -1);
+      MyDeque<Double> deque = new MyDeque<Double>();
+      for (int i=0; i<vals.length; i++){
+        if (vals[i] != "*" && vals[i] != "/" && vals[i] != "+" && vals[i] != "-"){
+          deque.addLast(Double.parseDouble(vals[i]));
+        } else {
+          if (vals[i] == "*"){
+
+          }
+        }
+      }
       return 0;
     }
 }
